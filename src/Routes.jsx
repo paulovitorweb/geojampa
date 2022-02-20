de381cde43cf1629caa0bf2c7304a82ff781cb1d
Paulo Freitas <paulogeo5@gmail.com>
2022-02-20 15:10:06 -0300

    Add default layer to router

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -7,9 +7,11 @@ import { Home } from './pages/Home'
 import layers from './constants/Layers'
 
 export function AppRoutes() {
+  const firstLayer = layers[0]
   return (
     <Router>
       <Routes>
+        <Route key={'first-layer'} path={'/'} element={<Home layer={firstLayer.key}/>} />
         {layers.map(layer => {
           const { key } = layer
           return <Route key={key} path={`/${key}`} element={<Home layer={key}/>} />
